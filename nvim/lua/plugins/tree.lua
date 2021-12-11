40cbe0bb3a1a47a5adad6b230e6a7af3366af811
Dzhabrail <dzhabulya@gmail.com>
2021-12-11 05:26:02 +0300

    Updated tree.lua

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -2,7 +2,7 @@ local g = vim.g;
 
 g.nvim_tree_respect_buf_cwd = 1
 g.nvim_tree_icon_padding = '  '
-g.nvim_tree_quit_on_open = 1
+-- g.nvim_tree_quit_on_open = 1
 g.nvim_tree_add_trailing = 1
 g.nvim_tree_show_icons = {
   git = 1,
@@ -14,11 +14,6 @@ g.nvim_tree_icons = {
   default = '',
 }
 require'nvim-tree'.setup {
-  -- update_cwd = true,
-  -- update_focused_file = {
-  --   enable = true,
-  --   update_cwd = true
-  -- },
   open_on_tab = true,
   system_open = {
     cmd  = "code",
